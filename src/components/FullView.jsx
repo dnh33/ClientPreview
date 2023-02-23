116494f8b02a7d6377bc5d0ce9181083e934173a
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-02-23 03:50:29 +0100

    rm allow scripts in sandbox etc

--- src/components/FullView.jsx
+++ src/components/FullView.jsx
@@ -10,7 +10,6 @@ export default function FullView(url) {
         src={url}
         width={781.846}
         height={586.385}
-        sandbox="allow-same-origin allow-scripts"
         className="inline-block border-0 m-0 p-0 overflow-hidden z-50
 "
       ></iframe>
@@ -19,7 +18,6 @@ export default function FullView(url) {
         src={url}
         width={401.869}
         height={537.128}
-        sandbox="allow-same-origin allow-scripts"
         className="inline-block border-0 m-0 p-0 overflow-hidden z-50
 "
       ></iframe>
@@ -28,7 +26,6 @@ export default function FullView(url) {
         src={url}
         width={185.558}
         height={365.904}
-        sandbox="allow-same-origin allow-scripts"
         className="inline-block border-0 m-0 p-0 overflow-hidden z-50
 "
       ></iframe>
