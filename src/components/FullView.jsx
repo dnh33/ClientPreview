632e674d52a0a73deea2945905725e71ff23b159
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-02-26 14:10:47 +0100

    add CSP middleware etc

--- src/components/FullView.jsx
+++ src/components/FullView.jsx
@@ -20,21 +20,28 @@ export default function FullView({ url }) {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen scale-box">
-      <div className="flex justify-center gap-8">
+    <div className="flex flex-column justify-end h-screen scale-box">
+      <div className="flex justify-between">
         {iframeUrls.map((iframeUrl, index) => (
-          <iframe
+          <Iframe
             key={index}
-            src={iframeUrl}
-            width={index === 0 ? 1200 : index === 1 ? 380.869 : 256}
-            height={index === 0 ? 800 : index === 1 ? 537.128 : 458}
+            url={iframeUrl}
+            width={index === 0 ? 1200 : index === 1 ? 480.869 : 256}
+            height={index === 0 ? 800 : index === 1 ? 737.128 : 458}
             className={`inline-block border-0 m-0 p-0 overflow-hidden z-50 
             ${index === 0 ? 'desktop-border' : ''}
             ${index === 1 ? 'tablet-border' : ''}
             ${index === 2 ? 'iphone-border' : ''}
           `}
-            style={{ alignSelf: 'flex-end' }}
-          ></iframe>
+            style={index === 0 ? { alignSelf: 'flex-end' } : {}}
+            userAgent={
+              index === 0
+                ? 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                : index === 1
+                ? 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+                : 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+            }
+          />
         ))}
       </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import helmet from 'helmet';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      // Enable React Refresh
+      fastRefresh: true,
+    }),
+    // Use Helmet middleware to set CSP headers
+    {
+      ...helmet.contentSecurityPolicy({
+        directives: {
+          defaultSrc: ["'self'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          scriptSrc: ["'self'"],
+          imgSrc: ["'self'", 'data:', 'blob:'],
+          fontSrc: ["'self'"],
+        },
+      }),
+      apply: 'serve',
+    },
+  ],
+});
