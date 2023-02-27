524ecd3032d5e3909296562ced4e1fc02e7e8a9f
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-02-27 01:37:43 +0100

    optimize

--- src/components/MobileView.jsx
+++ src/components/MobileView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function MobileView({ url }) {
   return (
     <center>
-      <div className="flex -pt-16 items-center justify-center h-screen rounded-lg">
+      <div className="flex p-24 h-screen rounded-lg scale-90">
         <iframe
           width={332}
           height={568}
