0371a73bf75e9fb7418fae18ad76c1391c64f969
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-03-02 21:16:44 +0100

    update CSP policy

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,16 @@ export default defineConfig({
         directives: {
           defaultSrc: ["'self'"],
           styleSrc: ["'self'", "'unsafe-inline'"],
-          scriptSrc: ["'self'"],
+          scriptSrc: ["'self'", 'venzo.com', 'webfair.dk', "'unsafe-inline'"],
           imgSrc: ["'self'", 'data:', 'blob:'],
           fontSrc: ["'self'"],
+          connectSrc: [
+            "'self'",
+            'vitals.vercel-insights.com',
+            'webfair.dk',
+            'h-plus.dk',
+            'tmmj.webfair.dk',
+          ],
         },
       }),
       apply: ['serve', 'build'],
