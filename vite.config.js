1138a62d0d602e6b4cdd4f2cc7438e5b0b20346b
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-03-03 16:25:48 +0100

    maybe this fixes it?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import { ViteMinifyPlugin } from 'vite-plugin-minify';
+/* import { ViteMinifyPlugin } from 'vite-plugin-minify'; */
 import react from '@vitejs/plugin-react';
-import helmet from 'helmet';
+/* import helmet from 'helmet'; */
 
 export default defineConfig({
   plugins: [
@@ -10,7 +10,7 @@ export default defineConfig({
       fastRefresh: true,
     }),
     // Use Helmet middleware to set CSP headers
-    {
+    /*  {
       ...helmet.contentSecurityPolicy({
         directives: {
           defaultSrc: ["'self'", 'data:'],
@@ -24,6 +24,6 @@ export default defineConfig({
       apply: ['serve', 'build'],
     },
     // input https://www.npmjs.com/package/html-minifier-terser options
-    ViteMinifyPlugin({}),
+    ViteMinifyPlugin({}), */
   ],
 });
