dd8cf8f0afcf1a0f6649d3ed9c00d88016e2ce3e
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-03-03 16:33:51 +0100

    update CSP agane agane

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-/* import { ViteMinifyPlugin } from 'vite-plugin-minify'; */
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import react from '@vitejs/plugin-react';
-/* import helmet from 'helmet'; */
+import helmet from 'helmet';
 
 export default defineConfig({
   plugins: [
@@ -10,20 +10,27 @@ export default defineConfig({
       fastRefresh: true,
     }),
     // Use Helmet middleware to set CSP headers
-    /*  {
+    {
       ...helmet.contentSecurityPolicy({
         directives: {
-          defaultSrc: ["'self'", 'data:'],
-          styleSrc: ["'self'", "'unsafe-inline'"],
-          scriptSrc: ["'self'", "'unsafe-inline'"],
+          defaultSrc: ["'self'"],
+          styleSrc: ["'self'", 'venzo.com', 'webfair.dk', "'unsafe-inline'"],
+          scriptSrc: ["'self'", 'venzo.com', 'webfair.dk', "'unsafe-inline'"],
           imgSrc: ["'self'", 'data:', 'blob:'],
-          fontSrc: ["'self'", 'data:'], // add data: protocol
-          connectSrc: ["'self'"],
+          fontSrc: ["'self'", 'venzo.com', 'webfair.dk'],
+          connectSrc: [
+            "'self'",
+            'webfair.dk',
+            'h-plus.dk',
+            'tmmj.webfair.dk',
+            'aexplore.io',
+            'h-plus.dk',
+          ],
         },
       }),
       apply: ['serve', 'build'],
     },
     // input https://www.npmjs.com/package/html-minifier-terser options
-    ViteMinifyPlugin({}), */
+    ViteMinifyPlugin({}),
   ],
 });
