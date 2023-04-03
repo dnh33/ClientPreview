cec8e0c94548286dfcd133284ea31712d3050b27
dnh33 <3074491+dnh33@users.noreply.github.com>
2023-04-04 00:44:59 +0200

    iframe optimization

--- src/components/FullView.jsx
+++ src/components/FullView.jsx
@@ -1,38 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Iframe from 'react-iframe';
 
 export default function FullView({ url }) {
   const [iframeUrls, setIframeUrls] = useState([url, url, url]);
   const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (inputValue) {
-      const newUrls = [inputValue, inputValue, inputValue];
-      setIframeUrls(newUrls);
-    } else {
-      const defaultUrls = [url, url, url];
-      setIframeUrls(defaultUrls);
-    }
+    setLoading(true);
+
+    const newUrls = inputValue
+      ? [inputValue, inputValue, inputValue]
+      : [url, url, url];
+    setIframeUrls(newUrls);
   }, [inputValue, url]);
 
-  const handleInputChange = (event) => {
+  const handleInputChange = useCallback((event) => {
     setInputValue(event.target.value);
-  };
+  }, []);
 
   return (
     <div className="flex items-center justify-center -mt-16 h-screen scale-box">
       <div className="flex justify-center gap-8">
         {iframeUrls.map((iframeUrl, index) => (
           <Iframe
+            key={index}
             url={iframeUrl}
             width={index === 0 ? 1280 : index === 1 ? 480 : 300}
             height={index === 0 ? 840 : index === 1 ? 738 : 558}
             className={`inline-block border-common m-0 p-0 overflow-hidden z-50 
-            ${index === 0 ? 'desktop-border' : ''}
-            ${index === 1 ? 'tablet-border' : ''}
-            ${index === 2 ? 'iphone-border' : ''}
-            
-          `}
+              ${index === 0 ? 'desktop-border' : ''}
+              ${index === 1 ? 'tablet-border' : ''}
+              ${index === 2 ? 'iphone-border' : ''}
+            `}
+            onLoad={() => setLoading(false)}
+            style={{ visibility: loading ? 'hidden' : 'visible' }}
           />
         ))}
       </div>
